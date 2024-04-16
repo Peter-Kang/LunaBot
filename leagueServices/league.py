331c9b07cb75eddda359aca4bd5c6c780cd57eb1
Peter Kang <pxk7062@rit.edu>
2024-04-15 23:52:14 -0400

    added a description for the past 7 days

--- leagueServices/league.py
+++ leagueServices/league.py
@@ -22,7 +22,7 @@ class league:
 
     async def getUserStatus(self, userID:str):
         sumStats:SummonerStat = await self.UserSummonerData.getStatus(userID)
-        result = f"WinRate: {sumStats.WinRate}\nTime Spent: {sumStats.TotalTimeSpent}\nGold Gained: {sumStats.TotalGold}\nMinions Merked: {sumStats.MinionsKilled}"
+        result = f"In the past 7 Days:\nWinRate: {sumStats.WinRate}\nTime Spent: {sumStats.TotalTimeSpent}\nGold Gained: {sumStats.TotalGold}\nMinions Merked: {sumStats.MinionsKilled}"
         return result
 
         
