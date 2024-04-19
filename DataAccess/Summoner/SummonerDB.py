00bbf7673999682f4f6a12e646f7a4b50da3cc76
Peter Kang <pxk7062@rit.edu>
2024-04-18 23:27:16 -0400

    removed unused call

--- DataAccess/Summoner/SummonerDB.py
+++ DataAccess/Summoner/SummonerDB.py
@@ -5,7 +5,7 @@ class SummonerDB(base):
     def __init__(self, sqliteConnection:sqlite3.Connection):
         super().__init__(sqliteConnection)
 
-    def InitUserToSummonerMapping(self):
+    def initUserToSummonerMapping(self):
         query:str = ''' CREATE TABLE IF NOT EXISTS UserToSummonerMapping (
 discordUserID TEXT PRIMARY KEY,
 summonerName TEXT NOT NULL,
@@ -16,11 +16,6 @@ PUUID TEXT);'''
         query:str="INSERT OR REPLACE INTO UserToSummonerMapping(discordUserID, summonerName, PUUID) VALUES(?,?,?);"
         args:tuple = (str(discordID),str(summonerName),str(PUUID))
         super().sendVoidCommand(query, args)
-
-    def getUserPUUID(self, discordID:str):
-        query:str = "SELECT discordUserID, summonerName, PUUID FROM UserToSummonerMapping WHERE discordUserID = ?;"
-        args:tuple = (str(discordID))
-        return super().sendScalarCommand(query, args)
 
     def getAllUsers(self):
         query:str = "SELECT discordUserID, PUUID FROM UserToSummonerMapping;"
