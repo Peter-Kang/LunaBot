ecc4d1865dc3a414a1d3f9fc714b60c9471edf6d
Peter Kang <pxk7062@rit.edu>
2024-04-20 22:36:25 -0400

    Added commands to get all matches

--- DataAccess/Matches/MatchesDB.py
+++ DataAccess/Matches/MatchesDB.py
@@ -1,4 +1,5 @@
 from DataAccess.Util.LeagueDBCommandBase import LeagueDBCommandBase as base
+from datetime import datetime, timedelta
 import sqlite3
 
 class MatchesDB(base):
@@ -6,16 +7,20 @@ class MatchesDB(base):
         super().__init__(sqliteConnection)
 
     def initMatchesTable(self) -> None:
-        query:str = "CREATE TABLE IF NOT EXISTS MatchCache ( MatchID TEXT PRIMARY KEY, EndTimeSecondsPastEpoch INTEGER NOT NULL ,Data TEXT NOT NULL )"
+        query:str = "CREATE TABLE IF NOT EXISTS MatchCache ( MatchID TEXT PRIMARY KEY, EndTimeSecondsPastEpoch INTEGER NOT NULL, Data TEXT NOT NULL );"
         super().sendVoidCommand(query)
 
     def addMatches(self, matchID:str, EndTimeSecondsPastEpoch:int, Data:str):
-        query:str="INSERT OR REPLACE INTO MatchCache(discordUserID, summonerName, PUUID) VALUES(?,?,?);"
+        query:str="INSERT OR REPLACE INTO MatchCache(MatchID, EndTimeSecondsPastEpoch, Data) VALUES(?,?,?);"
         args:tuple = (matchID,EndTimeSecondsPastEpoch,Data)
         super().sendVoidCommand(query, args)
 
     def clearPastDays(self, days:int) -> None:
-        pass
+        epoch = int((datetime.now() - timedelta(days=days)).timestamp())
+        query:str = "DELETE FROM MatchCache WHERE EndTimeSecondsPastEpoch < ?;"
+        args:tuple = (epoch)
+        super().sendVoidCommand(query, args)
 
-    def getMatches(self, listOfMatches:tuple) -> list:
-        pass
+    def getMatches(self) -> list:
+        query:str = "SELECT MatchID, EndTimeSecondsPastEpoch, Data FROM MatchCache;"
+        return super().sendQueryCommand(query)
