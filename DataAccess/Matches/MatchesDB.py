9a99f0a2fa712e86532dbb4c590a219380121c50
Peter Kang <pxk7062@rit.edu>
2024-04-20 22:21:26 -0400

    Added init matches and addmatches

--- DataAccess/Matches/MatchesDB.py
+++ DataAccess/Matches/MatchesDB.py
@@ -5,15 +5,17 @@ class MatchesDB(base):
     def __init__(self, sqliteConnection:sqlite3.Connection):
         super().__init__(sqliteConnection)
 
-    def initMatchesTable(self):
-        query:str = "CREATE TABLE IF NOT EXISTS "
+    def initMatchesTable(self) -> None:
+        query:str = "CREATE TABLE IF NOT EXISTS MatchCache ( MatchID TEXT PRIMARY KEY, EndTimeSecondsPastEpoch INTEGER NOT NULL ,Data TEXT NOT NULL )"
+        super().sendVoidCommand(query)
+
+    def addMatches(self, matchID:str, EndTimeSecondsPastEpoch:int, Data:str):
+        query:str="INSERT OR REPLACE INTO MatchCache(discordUserID, summonerName, PUUID) VALUES(?,?,?);"
+        args:tuple = (matchID,EndTimeSecondsPastEpoch,Data)
+        super().sendVoidCommand(query, args)
+
+    def clearPastDays(self, days:int) -> None:
         pass
 
-    def addOrIgnoreMatches(self):
-        pass
-
-    def clearPastDays(self, days:int):
-        pass
-
-    def getMatches(self, listOfMatches:tuple):
+    def getMatches(self, listOfMatches:tuple) -> list:
         pass
