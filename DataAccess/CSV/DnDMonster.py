a476a882d60e604559edb91a77942ddeb2c22774
Peter Kang <pxk7062@rit.edu>
2024-05-01 13:17:32 -0400

    Added new line for each stat

--- DataAccess/CSV/DnDMonster.py
+++ DataAccess/CSV/DnDMonster.py
@@ -114,7 +114,7 @@ class DnDMonster:
                         value="\n".join(movementList),
                         inline=False)
         embed.add_field(name="Stats",
-                        value=f">  STR: {self.Strength} DEX: {self.Dexterity} CON: {self.Constitution} INT: {self.Intelligence} WIS: {self.Wisdom} CHA: {self.Charisma}",
+                        value=f">  STR: {self.Strength}\nDEX: {self.Dexterity}\nCON: {self.Constitution}\nINT: {self.Intelligence}\nWIS: {self.Wisdom}\nCHA: {self.Charisma}",
                         inline=False)
         embed.add_field(name="Details",
                         value=f">  Saving Throws: {self.SavingThrows}\n>  Skills: {self.Skills}\n>  WRI: {self.WeaknessResistanceImmunities}\n>  Senses: {self.Senses}\n>  Languages: {self.Languages}",
