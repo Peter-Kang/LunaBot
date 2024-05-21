bb86742ad73faa81ba40093874f36349b57d6ad5
Peter Kang <pxk7062@rit.edu>
2024-05-21 12:38:17 -0400

    Updated Roll command to take in +'s

--- Services/DnDServices/DnDService.py
+++ Services/DnDServices/DnDService.py
@@ -9,21 +9,26 @@ class DnD:
         self.DnDMonsters:DnDMonsters = DnDMonsters()
 
     def roll(self, input:str)->str:
-        diceInfo:list[int] = input.lower().replace(" ","").split('d') #ie 2d8
+        diceInfo:list[str] = input.lower().replace(" ","").split('d') #ie 2d8
         inputParsed:str = input.lower().replace(" ","")
         result:str = "Please enter valid dice format. ie: 2d8 is 2 eight sided dice."
-        if(len(diceInfo) == 2 and diceInfo[0].isnumeric() and diceInfo[1].isnumeric() and int(diceInfo[0]) > 0 and int(diceInfo[1]) >0 ):
+        if(len(diceInfo) == 2 and diceInfo[0].isnumeric() and int(diceInfo[0]) > 0):
             count:int = int(diceInfo[0])
-            diceSide:int = int(diceInfo[1])+1
-            rollResults:list[int] = []
-            total:int = 0
-            for _ in range(0,count):
-                roll:int = random.randrange(1, diceSide, 1)
-                rollResults.append(roll)
-                total += roll
-            rollResultsString = ' , '.join([str(i) for i in rollResults])
-            
-            result=f"You Rolled {inputParsed}\n[{rollResultsString}]\nTotal:{total}"
+            diceSide:int = 0
+            flatAdd:int = 0
+            secondHalf:list[str] = diceInfo[1].split('+')
+            if((len(secondHalf) == 1 and secondHalf[0].isnumeric() and int(secondHalf[0])>0) or (len(secondHalf) == 2 and secondHalf[0].isnumeric() and secondHalf[1].isnumeric() and int(secondHalf[0]) >0 )):
+                diceSide = int(secondHalf[0])+1
+                if(len(secondHalf) == 2):
+                    flatAdd = int(secondHalf[1])
+                rollResults:list[int] = []
+                total:int = 0
+                for _ in range(0,count):
+                    roll:int = random.randrange(1, diceSide, 1) + flatAdd
+                    rollResults.append(roll)
+                    total += roll 
+                rollResultsString = ' , '.join([str(i) for i in rollResults])
+                result=f"You Rolled {inputParsed}\n[{rollResultsString}]\nTotal:{total}"
         return result
 
     def Encounter(self, ChallengeRating:float =-1.0, Environment:DnDEnvironments=DnDEnvironments.All) -> discord.Embed:
