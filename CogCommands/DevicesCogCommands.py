9aeb2a2d86c3efedd7283f7f046c8f1a12cde83f
Peter-Kang <pkang93@gmail.com>
2024-09-11 18:27:15 -0400

    Added command for temperature and humidity

---
+++ CogCommands/DevicesCogCommands.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from Services.HALService.RaspberryPi.RaspberryPi import RPI
+
+class DevicesCogCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="temperature", description="Gets the temperature and humidity")
+    async def temperature(self,interaction:discord.Interaction):
+        result:str = self.bot.RPIService.GetTemperatureAndHumidity()
+        await interaction.response.send_message(result)
+
+async def setup(bot:commands.Bot) ->None:
+    await bot.add_cog(DevicesCogCommands(bot=bot))
