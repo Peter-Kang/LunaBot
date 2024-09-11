b7ba37734719052316ffa047d7b5c418b3419f71
Peter-Kang <pkang93@gmail.com>
2024-09-11 18:22:28 -0400

    Updated Drivers

--- LeagueDiscordBot.py
+++ LeagueDiscordBot.py
@@ -8,7 +8,7 @@ from Services.LeagueServices.league import league
 from DataAccess.LeagueDatabase import LeagueDatabase
 from Services.DnDServices.DnDService import DnD
 
-from Services.HALService.RaspberryPi import RPI
+from Services.HALService.RaspberryPi.RaspberryPi import RPI
 
 class LeagueDiscordBot(commands.Bot):
 
